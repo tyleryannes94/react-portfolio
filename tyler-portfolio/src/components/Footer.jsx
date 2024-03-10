4bb5a4009b1f4c4f162cff8216dd90b563baf4c0
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-09 18:39:58 -0600

    updated a basic footer component; needs styling

--- tyler-portfolio/src/components/Footer.jsx
+++ tyler-portfolio/src/components/Footer.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer>
+      <p><strong>Email:</strong> <a href="mailto:tyler.yannes@gmail.com">tyler.yannes@gmail.com</a></p>
+      <p> <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/tyler-yannes" >Tyler's LinkedIn</a> </p>
+      <p> <strong>Github:</strong> <a href="https://github.com/tyleryannes94" >Tyler's Github</a> </p>
+      <p> <strong>Employer:</strong> <a href="https://skio.com/team/">Skio</a></p>
+    </footer>
+  );
 }
