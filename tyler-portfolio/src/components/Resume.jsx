12485e4d561f740539792493b1d588b55ba2079b
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-10 20:51:39 -0500

    Added embedded resume to resume component

--- tyler-portfolio/src/components/Resume.jsx
+++ tyler-portfolio/src/components/Resume.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 export default function Resume() {
+  // Construct the embed URL from the shareable link
+  const embedUrl = "https://docs.google.com/document/d/1DFORBQbXTY6P9iXSNTRfmwu1jbSp09W5bZoCDbkU6OU/edit";
+
+  // Download URL for the resume - you might need to upload a PDF version somewhere accessible and use that URL here
+  const downloadUrl = "https://docs.google.com/document/d/1DFORBQbXTY6P9iXSNTRfmwu1jbSp09W5bZoCDbkU6OU/edit";
+
   return (
-    <div>
-      
+    <div className="resume-container">
+      <h2 className="text-center my-4">My Resume</h2>
+      {/* Embed the Google Doc */}
+      <iframe src={embedUrl} width="100%" height="600px" frameborder="0"></iframe>
+      {/* Provide a download link */}
+      <div className="text-center mt-4">
+        <a href={downloadUrl} target="_blank" rel="noopener noreferrer" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Download Resume
+        </a>
+      </div>
     </div>
-  )
+  );
 }
