d97290f27502f89af4d9464bc40edd4cdf4bac66
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-10 19:52:48 -0500

    added react tailwind css styling to footer component

--- tyler-portfolio/src/components/Footer.jsx
+++ tyler-portfolio/src/components/Footer.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer>
-      <p><strong>Email:</strong> <a href="mailto:tyler.yannes@gmail.com">tyler.yannes@gmail.com</a></p>
-      <p> <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/tyler-yannes" >Tyler's LinkedIn</a> </p>
-      <p> <strong>Github:</strong> <a href="https://github.com/tyleryannes94" >Tyler's Github</a> </p>
-      <p> <strong>Employer:</strong> <a href="https://skio.com/team/">Skio</a></p>
+    <footer className="bg-zinc-50 text-center text-surface/75 dark:bg-neutral-700 dark:text-white/75 lg:text-left">
+      <div className="mx-6 py-10 text-center md:text-left">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+          <div>
+            <p><strong>Email:</strong> <a href="mailto:tyler.yannes@gmail.com" className="text-blue-600 hover:text-blue-800">tyler.yannes@gmail.com</a></p>
+          </div>
+          <div>
+            <p> <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/tyler-yannes" className="text-blue-600 hover:text-blue-800">Tyler's LinkedIn</a> </p>
+          </div>
+          <div>
+            <p> <strong>Github:</strong> <a href="https://github.com/tyleryannes94" className="text-blue-600 hover:text-blue-800">Tyler's Github</a> </p>
+          </div>
+          <div>
+            <p> <strong>Employer:</strong> <a href="https://skio.com/team/" className="text-blue-600 hover:text-blue-800">Skio</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-black/5 p-6 text-center">
+        <span>© 2024 "Copyright":</span>
+        <a className="font-semibold" href="https://tw-elements.com/">TW Elements</a>
+      </div>
     </footer>
   );
 }
