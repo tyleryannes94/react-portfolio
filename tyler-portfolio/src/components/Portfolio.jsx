b9e126ace8fbd1d81d978f4badcd98c9ddde7b33
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-10 21:26:20 -0500

    added 2 more projects to my portfolio and finished the design

--- tyler-portfolio/src/components/Portfolio.jsx
+++ tyler-portfolio/src/components/Portfolio.jsx
@@ -1,12 +1,29 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import resoluteTrackerImage from '../assets/resolute-tracker.png';
-import studyGuideImage from '../assets/prework-study-guide.png';
-import codingCertificateImage from '../assets/coding-certificate.png';
-import publicSpeakingImage from '../assets/public-speaking.jpg';
+import project3Image from '../assets/project-3.png'; 
+import module18Image from '../assets/module-18.png'; 
+import project1Image from '../assets/project-1.png'; 
+import module13Image from '../assets/module-13.png'; 
+import module12Image from '../assets/module-12.png'; 
+
+
+
+
 
 export default function Portfolio() {
   const [hoveredItem, setHoveredItem] = useState(null);
+
+  const projects = [
+    { img: project1Image, name: 'Project 1', link: 'https://github.com/Donnie1129/marvel-character-personality-quiz' },
+    { img:  resoluteTrackerImage, name: 'Project 2', link: 'https://github.com/tyleryannes94/health-tracker' }, 
+    { img: project3Image, name: 'Project 3', link: 'https://github.com/tyleryannes94/workout-buddy' },
+    { img: module12Image, name: 'Module 12', link: 'https://github.com/tyleryannes94/social-media-api' },
+    { img: module13Image, name: 'Module 13', link: 'https://github.com/tyleryannes94/ecommerce-backend' },
+    { img: module18Image, name: 'Module 18', link: 'https://github.com/tyleryannes94/social-media-api' },
+
+  ];
+
 
   const applyBlur = (index) => hoveredItem !== null && hoveredItem !== index ? 'filter blur-sm' : '';
 
@@ -23,7 +40,7 @@ export default function Portfolio() {
       >
         <h1 className="text-3xl font-bold mb-12">My Work</h1>
         <div className="flex flex-wrap justify-center">
-          {[resoluteTrackerImage, studyGuideImage, codingCertificateImage, publicSpeakingImage].map((image, index) => (
+          {projects.map((project, index) => (
             <motion.div
               key={index}
               onMouseEnter={() => setHoveredItem(index)}
@@ -32,10 +49,10 @@ export default function Portfolio() {
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
             >
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <img src={image} alt={`Project ${index + 1}`} className="rounded-lg shadow-lg dark:shadow-black/20" />
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                <img src={project.img} alt={project.name} className="rounded-lg shadow-lg dark:shadow-black/20" />
               </a>
-              <h3>Project {index + 1}</h3>
+              <h3>{project.name}</h3>
             </motion.div>
           ))}
         </div>
